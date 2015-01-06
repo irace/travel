b12281876b804e14e9a8ec5a78a8b757eeea0046
Bryan Irace <bryan.irace@gmail.com>
2015-01-05 23:08:47 -0500

    Countries

---
+++ scripts/mapping.js
@@ -0,0 +1,63 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiaXJhY2UiLCJhIjoiOFRlNWJSOCJ9.DnNeo9t7r0a5irSR1Be4_Q';
+
+var map = L.mapbox.map('map-one', 'irace.km1dk6m1', {
+  attributionControl: false        
+});
+
+var processData = function (error, results) {
+  var stateData   = results[0] 
+    , countryData = results[1]
+    , travelData  = results[2];
+
+  var addOverlaysToMap = function (geoJSON, fillColor, filter) {
+    L.geoJson(geoJSON, {
+      style: function () {
+        return {
+          weight: 1,
+          opacity: 0.8,
+          color: 'darkgray',
+          fillOpacity: 0.7,
+          fillColor: fillColor
+        };
+      },
+      filter: filter
+    }).addTo(map);
+  };
+
+  var visitedCountryIDs = _(travelData.countries).pluck('id');
+
+  addOverlaysToMap(countryData, '#FF9B9B', function (feature) {
+    return _(visitedCountryIDs).contains(feature.id);
+  });
+
+  var visitedStateIDs = _(travelData.countries)
+    .chain()
+    .map(function (country) {
+      return _(country.states).pluck('id');
+    })
+    .flatten()
+    .value();
+
+  addOverlaysToMap(stateData, '#36455c', function (feature) {
+    return _(visitedStateIDs).contains(feature.properties.STATE);
+  });
+
+  var message = _.size(visitedStateIDs) + ' states, ' + _.size(visitedCountryIDs) + ' countries visited';
+
+  map.addControl(L.mapbox.legendControl().addLegend(message));
+};
+
+// Fetch travel data and state/country GeoJSON
+
+(function fetchData() {
+  // Create an array of curried functions suitable to pass into `async.parallel`
+  var dataFetchers = _(['states.geojson', 'countries.geojson', 'travel.json']).map(function (fileName) {
+    return function (callback) {
+      $.getJSON('data/' + fileName, function (data) {
+        callback(null, data);
+      });
+    };
+  });
+
+  async.parallel(dataFetchers, processData);
+}());
